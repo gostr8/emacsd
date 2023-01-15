295d948d9ea36f9c088109f7811b96bb943e2f48
gostr8 <gostr8@bukchon.dev>
2023-01-15 18:56:30 +0900

    .

--- init.el
+++ init.el
@@ -3,7 +3,13 @@
 ;; 
 ;; without MELPA things..
 ;; gostr8
+(add-to-list 'load-path "~/.emacs.d/site-lisp/magit/lisp")
+(require 'magit)
 
+(with-eval-after-load 'info
+  (info-initialize)
+  (add-to-list 'Info-directory-list
+               "~/.emacs.d/site-lisp/magit/Documentation/"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
