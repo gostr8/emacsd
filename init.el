e7899ce98ef3151b3f45fcc1fd1b8d4e7f654865
gostr8 <gostr8@bukchon.dev>
2023-01-15 20:07:27 +0900

    magit, use-package

--- init.el
+++ init.el
@@ -4,12 +4,14 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-
+;; for use-package...
+(if (not (package-installed-p 'use-package))
+    (progn
+      (package-refresh-contents)
+      (package-install 'use-package)))
+(require 'use-package)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
