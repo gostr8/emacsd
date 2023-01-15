5603068227667114e1a8506fd6676cad7e445996
gostr8 <gostr8@bukchon.dev>
2023-01-15 22:05:45 +0900

    init.el

--- init.el
+++ init.el
@@ -1,6 +1,15 @@
 ;; Start emacs on maximzed frame.
 (add-hook 'emacs-startup-hook 'toggle-frame-maximized)
 
+
+;; fonts and locales and.. languages..
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+
 ;; set shell path for macosx
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
@@ -9,6 +18,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
 
 ;; for use-package...
 (if (not (package-installed-p 'use-package))
@@ -20,13 +30,17 @@
 ;; Theme
 (use-package gruvbox-theme
   :ensure t
-  :config
-  (load-theme 'gruvbox-dark-soft t))
+  :config     (load-theme 'gruvbox-dark-soft t))
 
 ;; line number
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
-;; autopair
+;; elisp auto formatting...
+(use-package aggressive-indent
+  :ensure t
+  :init
+  (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+  )
 
 
 ;; lsp-mode
@@ -92,7 +106,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(exec-path-from-shell magit)))
+ '(package-selected-packages '(aggressive-indent exec-path-from-shell magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
